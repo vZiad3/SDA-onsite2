4e214bd02195805be3c04c838e2e89f78a3454e4
Ziad alshammary <ziad53@Ziads-MacBook-Pro.local>
2024-03-04 00:16:01 +0300

    sunday22

--- src/test/java/day08/registrationChallenge.java
+++ src/test/java/day08/registrationChallenge.java
@@ -19,32 +19,38 @@ public class registrationChallenge extends TestBase {
         driver.switchTo().frame(iframe);
         driver.findElement(By.xpath("//*[@class='fmSmtButton submitColor submitWrapper fmSmtButtom']")).click();
 
-
-        //Insert Fname
+        //Verify the error message when leaving First name  || Last name empty
         WebElement nameError = driver.findElement(By.id("error-Name"));
         Assert.assertTrue(nameError.isDisplayed());
 
+        //Insert Fname
         WebElement fName = driver.findElement(By.xpath("//*[@aria-labelledby='Name-arialabel aria-showelemslabel-Name0 ariarequired-Name0']"));
         fName.sendKeys("Ziad");
-
-
 
         //insert Lname
         WebElement lName = driver.findElement(By.xpath("//*[@aria-labelledby='Name-arialabel aria-showelemslabel-Name1 ariarequired-Name1']"));
         lName.click();
         lName.sendKeys("alsh");
 
-        //Click radioButton the First button of the radiobutton
-        driver.findElement(By.xpath("//*[@class='radioChoice cusChoiceLabel']//em[.='First Choice']")).click();
 
-        //DROP DOWN
+        //Click radioButton the First button of the radiobutton
+        WebElement radioButton = driver.findElement(By.xpath("//*[@class='radioChoice cusChoiceLabel']//em[.='First Choice']"));
+        radioButton.click();
+
+        //Verify radio button
+        Assert.assertTrue(radioButton.isDisplayed());
+
+        //Verify the error message when leaving Drop down empty
         WebElement dropError = driver.findElement(By.id("error-Dropdown"));
         Assert.assertTrue(dropError.isDisplayed());
+
+        //DROP DOWN select
         WebElement dropDown = driver.findElement(By.id("Dropdown-arialabel"));
         Select select = new Select(dropDown);
         select.selectByValue("Second Choice");
 
 
+        //Verify the not editable field
         try {
             // THE FIELD ISNT EDITABLE
             WebElement notEditable = driver.findElement(By.id("SingleLine-arialabel"));
@@ -58,9 +64,15 @@ public class registrationChallenge extends TestBase {
             Assert.assertTrue(true);
         }
 
-        //Clicking on Check box
 
-        driver.findElement(By.xpath(" //*[@id='Checkbox-li']//*[contains(text(),'Second Choice')]")).click();
+
+        //Clicking on Check box
+        WebElement checkChoise = driver.findElement(By.xpath(" //*[@id='Checkbox-li']//*[contains(text(),'Second Choice')]"));
+        checkChoise.click();
+
+        //Verify check box
+        Assert.assertNotNull("its null",checkChoise.isSelected());
+
 
         //Click on rating
         List<WebElement> ratingList = driver.findElements(By.xpath("//*[@aria-describedby='Rating-arialabel hint-Rating']"));
