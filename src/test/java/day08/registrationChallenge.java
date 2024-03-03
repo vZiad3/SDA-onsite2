ebefab04563ca1d0676f964d93566857807aa77c
Ziad alshammary <ziad53@Ziads-MacBook-Pro.local>
2024-03-04 00:05:07 +0300

    sunday2

--- src/test/java/day08/registrationChallenge.java
+++ src/test/java/day08/registrationChallenge.java
@@ -7,20 +7,27 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 import utilities.TestBase;
 
+import java.util.List;
+
 public class registrationChallenge extends TestBase {
 
     @Test
-    public void test1(){
+    public void test1() throws InterruptedException {
         driver.get("https://clarusway.getlearnworlds.com/sda-test-registration-form");
         //Locating iframe
         WebElement iframe = driver.findElement(By.xpath("//iframe[@aria-label='SDA Iframe Test']"));
         driver.switchTo().frame(iframe);
         driver.findElement(By.xpath("//*[@class='fmSmtButton submitColor submitWrapper fmSmtButtom']")).click();
 
+
         //Insert Fname
+        WebElement nameError = driver.findElement(By.id("error-Name"));
+        Assert.assertTrue(nameError.isDisplayed());
+
         WebElement fName = driver.findElement(By.xpath("//*[@aria-labelledby='Name-arialabel aria-showelemslabel-Name0 ariarequired-Name0']"));
-        fName.click();
         fName.sendKeys("Ziad");
+
+
 
         //insert Lname
         WebElement lName = driver.findElement(By.xpath("//*[@aria-labelledby='Name-arialabel aria-showelemslabel-Name1 ariarequired-Name1']"));
@@ -31,39 +38,44 @@ public class registrationChallenge extends TestBase {
         driver.findElement(By.xpath("//*[@class='radioChoice cusChoiceLabel']//em[.='First Choice']")).click();
 
         //DROP DOWN
-    WebElement dropDown = driver.findElement(By.id("Dropdown-arialabel"));
+        WebElement dropError = driver.findElement(By.id("error-Dropdown"));
+        Assert.assertTrue(dropError.isDisplayed());
+        WebElement dropDown = driver.findElement(By.id("Dropdown-arialabel"));
         Select select = new Select(dropDown);
         select.selectByValue("Second Choice");
 
 
-      try {
-          // THE FIELD ISNT EDITABLE
-          WebElement notEditable = driver.findElement(By.id("SingleLine-arialabel"));
-          notEditable.sendKeys("33");
+        try {
+            // THE FIELD ISNT EDITABLE
+            WebElement notEditable = driver.findElement(By.id("SingleLine-arialabel"));
+            notEditable.sendKeys("33");
 
-          Assert.fail("The field is editable, but it shouldn't be");
-          //String not = notEditable.getAttribute("true");
-          //Assert.as(not);
+            Assert.fail("The field is editable, but it shouldn't be");
+            //String not = notEditable.getAttribute("true");
+            //Assert.as(not);
 
-      }catch (Exception e){
-          Assert.assertTrue(true);
-      }
+        }catch (Exception e){
+            Assert.assertTrue(true);
+        }
 
-      //Clicking on Check box
+        //Clicking on Check box
 
         driver.findElement(By.xpath(" //*[@id='Checkbox-li']//*[contains(text(),'Second Choice')]")).click();
 
-        WebElement rating = driver.findElement(By.xpath(" //*[@aria-label='3 Star']"));
-        rating.click();
+        //Click on rating
+        List<WebElement> ratingList = driver.findElements(By.xpath("//*[@aria-describedby='Rating-arialabel hint-Rating']"));
+        ratingList.get(3).click();
+        //  WebElement rating = driver.findElement(By.xpath(" //*[@aria-label='3 Star']"));
+        //rating.click();
+
+        WebElement ratingCheck = driver.findElement(By.xpath("//*[@role='radio'][@class='ON']"));
+        Assert.assertEquals(ratingList,ratingCheck);
 
         driver.findElement(By.xpath("//*[@class='fmSmtButton submitColor submitWrapper fmSmtButtom']")).click();
 
+        //   driver.findElement(By.xpath("//*[@class='fmSmtButton submitColor submitWrapper fmSmtButtom']")).click();
+
         //*[@aria-labelledby='Name-arialabel aria-showelemslabel-Name1 ariarequired-Name1']
-
-
-
-
-
 
 
 
@@ -77,3 +89,5 @@ public class registrationChallenge extends TestBase {
 
 
 }
+
+
